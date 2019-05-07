c72fcc843ba9442f52247e7f4734928941ccaaa9
patfox807 <47128021+patfox807@users.noreply.github.com>
2019-05-06 21:18:42 -0400

    <new code>

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,18 @@
 var path = require("path");
 
 module.exports = function(app) {
-    app.get("/survey", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
 
-    app.get("*", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  app.get("/results", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/results.html"))
+  });
+
+ 
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 };
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require("express");
-var path = require('path');
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-require('./app/routing/apiRoutes');
-require('./app/routing/htmlRoutes');
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
+app.listen(PORT, function(){
+    console.log("App listenting on PORT: "+ PORT);
+})
